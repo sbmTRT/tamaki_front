17a20510fae08e46bd4dffe368990d187d1f9eef
sbmTRT <thureintun@signalbase.co.jp>
2023-12-06 17:26:12 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import {createApp} from 'vue';
 import App from './App.vue';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+createApp(App).mount('#app');
