fd72b05779eaa3f67b6aa42e19d408397cc28f31
sbmTRT <thureintun@signalbase.co.jp>
2023-12-06 13:27:49 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,6 @@
 
 <script>
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.slim.min.js';
-import 'popper.js/dist/umd/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import Header from './components/Header.vue';
