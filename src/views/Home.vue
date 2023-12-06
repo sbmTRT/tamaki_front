4087d07fa64dfbc48e2ee0e6e53f4a3caa54b8fe
sbmTRT <thureintun@signalbase.co.jp>
2023-12-06 17:40:03 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,64 @@
 <template>
-  <div class="container-fluid">
-    <h1>Dashboard</h1>
+  <div class="container mt-5">
+    <form @submit.prevent="submitForm">
+      <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input v-model="formData.name" type="text" class="form-control" id="name" placeholder="Enter your name">
+      </div>
+
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input v-model="formData.email" type="email" class="form-control" id="email" placeholder="name@example.com">
+      </div>
+
+      <div class="mb-3">
+        <label for="address" class="form-label">Address</label>
+        <input v-model="formData.address" type="text" class="form-control" id="address" placeholder="123 Main St">
+      </div>
+
+      <div class="mb-3">
+        <label for="city" class="form-label">City</label>
+        <input v-model="formData.city" type="text" class="form-control" id="city" placeholder="City">
+      </div>
+
+      <div class="mb-3">
+        <label for="state" class="form-label">State</label>
+        <input v-model="formData.state" type="text" class="form-control" id="state" placeholder="State">
+      </div>
+
+      <div class="mb-3">
+        <label for="zip" class="form-label">ZIP Code</label>
+        <input v-model="formData.zip" type="text" class="form-control" id="zip" placeholder="ZIP Code">
+      </div>
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  // View-specific logic goes here
-}
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        address: '',
+        city: '',
+        state: '',
+        zip: '',
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission logic here
+      alert(`Form submitted with data: ${JSON.stringify(this.formData)}`);
+    },
+  },
+};
 </script>
 
 <style scoped>
-/* View-specific styles go here */
+/* Add any custom styles for this component */
 </style>
