aeef374c6f73fd27b59d90e1655aaf74965ea9fd
sbmTRT <thureintun@signalbase.co.jp>
2023-12-15 13:33:40 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,49 +4,53 @@
         <div class="row">
           <div class="col-md-6 offset-md-3">
             <form @submit.prevent="submitForm" class="form-inline">
-            <div class="card">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-sm-3">
-                    <div class="form-group text-sm">
-                      <label for="name">Name</label>
-                      <input v-model="formData.name" type="text" class="form-control form-control-sm bg-mistyrose" id="name" placeholder="Enter your name">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <div class="form-group text-sm">
+                        <label for="name">Name</label>
+                        <input v-model="formData.name" type="text" class="form-control form-control-sm bg-mistyrose" id="name" placeholder="Enter your name">
+                      </div>
+                    </div>
+                    <div class="col-sm-3">
+                      <div class="form-group text-sm">
+                        <label for="email">Email address</label>
+                        <input v-model="formData.email" type="email" class="form-control form-control-sm bg-mistyrose" id="email" placeholder="name@example.com">
+                      </div>
                     </div>
                   </div>
-                  <div class="col-sm-3">
-                    <div class="form-group text-sm">
-                      <label for="email">Email address</label>
-                      <input v-model="formData.email" type="email" class="form-control form-control-sm bg-mistyrose" id="email" placeholder="name@example.com">
-                    </div>
                 </div>
               </div>
-            </div>
 
-            <div class="card">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-sm-3">
-                    <div class="form-group text-sm">
-                      <label for="address">Address</label>
-                      <input v-model="formData.address" type="text" class="form-control form-control-sm bg-mistyrose" id="address" placeholder="123 Main St">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <div class="form-group text-sm">
+                        <label for="address">Address</label>
+                        <input v-model="formData.address" type="text" class="form-control form-control-sm bg-mistyrose" id="address" placeholder="123 Main St">
+                      </div>
+                    </div>
+                    <div class="col-sm-3">
+                      <div class="form-group text-sm">
+                        <label for="city">City</label>
+                        <input v-model="formData.city" type="text" class="form-control form-control-sm bg-mistyrose" id="city" placeholder="City">
+                      </div>
                     </div>
                   </div>
-                  <div class="col-sm-3">
-                    <div class="form-group text-sm">
-                      <label for="city">City</label>
-                      <input v-model="formData.city" type="text" class="form-control form-control-sm bg-mistyrose" id="city" placeholder="City">
-                    </div>
                 </div>
               </div>
-            </div>
 
-            <div class="card">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-sm-3">
-                    <div class="form-group text-sm">
-                      <button type="submit" class="btn btn-primary w-25">Submit</button>
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <div class="form-group text-sm">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </div>
                     </div>
+                  </div>
                 </div>
               </div>
             </form>
