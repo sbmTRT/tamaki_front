bb3d432f154b0175b8319a056f35dfdbbef16dae
sbmTRT <thureintun@signalbase.co.jp>
2023-12-15 13:28:54 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,38 +4,45 @@
         <div class="row">
           <div class="col-md-6 offset-md-3">
             <form @submit.prevent="submitForm" class="form-inline">
-              <div class="form-group">
-                <label for="name" class="form-label col-sm-4">Name</label>
-                <input v-model="formData.name" type="text" class="form-control col-sm-8" id="name" placeholder="Enter your name">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-3">
+                    <div class="form-group text-sm">
+                      <label for="name">Name</label>
+                      <input v-model="formData.name" type="text" class="form-control form-control-sm bg-mistyrose" id="name" placeholder="Enter your name">
+                  <div class="col-sm-3">
+                    <div class="form-group text-sm">
+                  <label for="email">Email address</label>
+                  <input v-model="formData.email" type="email" class="form-control form-control-sm bg-mistyrose" id="email" placeholder="name@example.com">
+                </div>
               </div>
+            </div>
 
-              <div class="form-group">
-                <label for="email" class="form-label col-sm-4">Email address</label>
-                <input v-model="formData.email" type="email" class="form-control col-sm-8" id="email" placeholder="name@example.com">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-3">
+                    <div class="form-group text-sm">
+                      <label for="address">Address</label>
+                      <input v-model="formData.address" type="text" class="form-control form-control-sm bg-mistyrose" id="address" placeholder="123 Main St">
+                  </div>
+                  <div class="col-sm-3">
+                    <div class="form-group text-sm">
+                      <label for="city">City</label>
+                      <input v-model="formData.city" type="text" class="form-control form-control-sm bg-mistyrose" id="city" placeholder="City">
+                </div>
               </div>
+            </div>
 
-              <div class="form-group">
-                <label for="address" class="form-label col-sm-4">Address</label>
-                <input v-model="formData.address" type="text" class="form-control col-sm-8" id="address" placeholder="123 Main St">
-              </div>
-
-              <div class="form-group">
-                <label for="city" class="form-label col-sm-4">City</label>
-                <input v-model="formData.city" type="text" class="form-control col-sm-8" id="city" placeholder="City">
-              </div>
-
-              <div class="form-group">
-                <label for="state" class="form-label col-sm-4">State</label>
-                <input v-model="formData.state" type="text" class="form-control col-sm-8" id="state" placeholder="State">
-              </div>
-
-              <div class="form-group">
-                <label for="zip" class="form-label col-sm-4">ZIP Code</label>
-                <input v-model="formData.zip" type="text" class="form-control col-sm-8" id="zip" placeholder="ZIP Code">
-              </div>
-
-              <div class="form-group">
-                <button type="submit" class="btn btn-primary col-sm-4 offset-sm-4">Submit</button>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-3">
+                    <div class="form-group text-sm">
+                      <button type="submit" class="btn btn-primary w-25">Submit</button>
+                    </div>
+                </div>
               </div>
             </form>
           </div><!-- /.col -->
