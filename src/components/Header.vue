aa9983e23100a3a85f7c71a57f2ea02d7f129a20
sbmTRT <thureintun@signalbase.co.jp>
2023-12-19 18:02:50 +0900

    initial commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,45 +3,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
   <!-- Container wrapper -->
   <div class="container-fluid">
-    <!-- Toggle button -->
-    <button
-      data-mdb-collapse-init
-      class="navbar-toggler"
-      type="button"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Navbar brand -->
-      <a class="navbar-brand mt-2 mt-lg-0" href="#">
-        <img
-          src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-          height="15"
-          alt="MDB Logo"
-          loading="lazy"
-        />
-      </a>
-      <!-- Left links -->
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-      <!-- Left links -->
+    <div class="row col-md-12">
+      <img src="../assets/img/logo.jpeg" alt="Logo of Tamaki System">
+      <h5>玉城町空き家バンク登録システム</h5>
     </div>
-    <!-- Collapsible wrapper -->
   </div>
   <!-- Container wrapper -->
 </nav>
