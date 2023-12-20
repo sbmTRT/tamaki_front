cf23e7eac319255e925c856edee39589668bc3ca
sbmTRT <thureintun@signalbase.co.jp>
2023-12-20 16:02:49 +0900

    initial commit

--- src/views/entry/Register.vue
+++ src/views/entry/Register.vue
@@ -1,21 +1,21 @@
 <template>
-<div class="container-fluid col-sm-9 mt-1 mb-5">
+<div class="container-fluid col-sm-6 mt-1 mb-5">
 <div class="card">
   <div class="card-body">
     <div class="row mb-2 mt-5">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">登録申込</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12">登録申込</button>
     </div>
     <div class="row mb-2">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">登録事項変更届出</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12">登録事項変更届出</button>
     </div>
     <div class="row mb-2">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">登録抹消申出</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12">登録抹消申出</button>
     </div>
     <div class="row mb-10">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">利用希望申込</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12">利用希望申込</button>
     </div>
     <div class="row mb-2 col-sm-6 ml-0 mt-5">
-        <button type="button" class="btn btn-secondary btn-sm shadow-sm col-sm-3">閉じる</button>
+        <button type="button" class="btn btn-secondary btn-sm shadow-sm col-sm-12">閉じる</button>
     </div>
   </div>
 </div>
