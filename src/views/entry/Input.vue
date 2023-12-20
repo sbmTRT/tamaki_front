4380e720d02642e28095da04abde41b32432a4e9
sbmTRT <thureintun@signalbase.co.jp>
2023-12-20 17:48:23 +0900

    initial commit

--- src/views/entry/Input.vue
+++ src/views/entry/Input.vue
@@ -1,30 +1,37 @@
 <template>
-<body class="container-sm mt-5">
-    <form>
-        <h2 class="text-center">
-            Display Form Controls
-        </h2>
-        <div class="form-group">
-            <label>Email address</label>
-            <input type="email" class="form-control"
-                placeholder="geeks@example.com">
+<div class="container-sm mt-5">
+    <div class="card mt-5">
+        <div class="card-header">
+            登録申込
         </div>
-        <div class="form-group mt-2">
-            <label>Example select</label>
-            <select class="form-control">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
+        <div class="card-body">
+            <form>
+                <h2 class="text-center">
+                    申込者
+                </h2>
+                <div class="form-group">
+                    <label>Email address</label>
+                    <input type="email" class="form-control"
+                        placeholder="geeks@example.com">
+                </div>
+                <div class="form-group mt-2">
+                    <label>Example select</label>
+                    <select class="form-control">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+                <div class="form-group mt-2">
+                    <label>Example textarea</label>
+                    <textarea class="form-control"></textarea>
+                </div>
+            </form>
         </div>
-        <div class="form-group mt-2">
-            <label>Example textarea</label>
-            <textarea class="form-control"></textarea>
-        </div>
-    </form>
-</body>
+    </div>
+</div>
 </template>
 
 <script>
