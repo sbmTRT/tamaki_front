0c15c769ae5ecc89ff9d3c8b0f890ac7de96f9db
sbmTRT <thureintun@signalbase.co.jp>
2023-12-20 13:54:09 +0900

    initial commit

--- src/views/entry/Register.vue
+++ src/views/entry/Register.vue
@@ -1,176 +1,20 @@
 <template>
 <div class="container-fluid col-sm-9 mt-1 mb-5">
-<form @submit.prevent="submitForm">
-  <div class="card">
-    <div class="card-body">
-      <div class="row">
-          <div class="col-sm-3">
-              <div class="form-group text-sm">
-                  <label for="division">区分</label>
-                  <input type="text" id="division" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-              </div>
-          </div>
-          <div class="col-sm-3">
-              <div class="form-group text-sm">
-                  <label for="processingDate">処理日</label>
-                  <input type="text" id="processingDate" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-              </div>
-          </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>支社局</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>事務所</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>道路</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>ＩＣ(自)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>ＩＣ(至)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>盛土番号</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>上下線区分</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>ルート区分</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="form-group text-sm">
-            <label>完成暫定区分</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-      </div>
-    </div>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
-  <div class="card mt-3">
-    <div class="card-body">
-      <div class="row">
-        <label class="text-info">管理</label>
-      </div>
-      <div class="row">
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＫＰ(自)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＫＰ(至)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ランプ</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＮＯ(自)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＮＯ(至)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
-  <div class="card mt-3">
-    <div class="card-body">
-      <div class="row">
-        <label class="text-info">建設</label>
-      </div>
-      <div class="row">
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＳＴＡ(自)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＳＴＡ(至)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ランプ</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＮＯ(自)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-2">
-          <div class="form-group text-sm">
-            <label>ＮＯ(至)</label>
-            <input type="text" class="form-control form-control-sm bg-mistyrose" readonly="readonly" />
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="form-group text-sm">
-            <label>支社局</label>
-            <input type="text" class="form-control form-control-sm bg-lightcyan" readonly="readonly" />
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="form-group text-sm">
-            <label>事務所</label>
-            <input type="text" class="form-control form-control-sm bg-lightcyan" readonly="readonly" />
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
 </template>
