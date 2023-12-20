ee00e00143b8decefa7fca7c318cf5a1d8a47efa
sbmTRT <thureintun@signalbase.co.jp>
2023-12-20 14:10:09 +0900

    initial commit

--- src/views/entry/Register.vue
+++ src/views/entry/Register.vue
@@ -1,9 +1,15 @@
 <template>
-<div class="container-fluid mt-1 mb-5">
-    <div class="row">
+<div class="container-fluid mt-1 mb-5 col-sm-9">
+    <div class="row mb-2">
         <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">登録申込</button>
+    </div>
+    <div class="row mb-2">
         <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">登録事項変更届出</button>
+    </div>
+    <div class="row mb-2">
         <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">登録抹消申出</button>
+    </div>
+    <div class="row mb-2">
         <button type="button" class="btn btn-outline-success shadow-sm col-sm-3">利用希望申込</button>
     </div>
 </div>
