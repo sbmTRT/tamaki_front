e2af3689c16b611fe3e628458c6fdfb5e969de39
sbmTRT <thureintun@signalbase.co.jp>
2023-12-20 17:55:43 +0900

    initial commit

--- src/views/entry/Input.vue
+++ src/views/entry/Input.vue
@@ -1,35 +1,41 @@
 <template>
-<div class="container-sm mt-5">
+<body class="container-sm mt-5">
     <div class="card mt-5">
-        <div class="card-header">
+        <div class="card-header bg-success text-white">
             登録申込
         </div>
         <div class="card-body">
-            <form>
-                <label class="text-left">申込者</label>
-                <div class="form-group">
-                    <label>Email address</label>
-                    <input type="email" class="form-control"
-                        placeholder="geeks@example.com">
+            <div class="card mt-5">
+                <div class="card-header text-green">
+                    申込者
                 </div>
-                <div class="form-group mt-2">
-                    <label>Example select</label>
-                    <select class="form-control">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control"
+                                placeholder="geeks@example.com">
+                        </div>
+                        <div class="form-group mt-2">
+                            <label>Example select</label>
+                            <select class="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                        <div class="form-group mt-2">
+                            <label>Example textarea</label>
+                            <textarea class="form-control"></textarea>
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group mt-2">
-                    <label>Example textarea</label>
-                    <textarea class="form-control"></textarea>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
-</div>
+</body>
 </template>
 
 <script>
