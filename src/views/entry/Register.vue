74c7dd32c9ea3ab4335474e053f6eca47fd70ccc
sbmTRT <thureintun@signalbase.co.jp>
2023-12-20 16:54:44 +0900

    initial commit

--- src/views/entry/Register.vue
+++ src/views/entry/Register.vue
@@ -3,16 +3,16 @@
 <div class="card">
   <div class="card-body">
     <div class="row mb-2 mt-5">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectToInput">登録申込</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('input')">登録申込</button>
     </div>
     <div class="row mb-2">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12">登録事項変更届出</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('update')">登録事項変更届出</button>
     </div>
     <div class="row mb-2">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12">登録抹消申出</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('delete')">登録抹消申出</button>
     </div>
     <div class="row mb-10">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12">利用希望申込</button>
+        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('application')">利用希望申込</button>
     </div>
     <div class="row mb-2 col-sm-6 ml-0 mt-5">
         <button type="button" class="btn btn-secondary btn-sm shadow-sm col-sm-12">閉じる</button>
@@ -37,8 +37,8 @@ export default {
     };
   },
   methods: {
-    redirectToInput() {
-      this.$router.push('/input');
+    redirectTo(routeName) {
+      this.$router.push({ name: routeName });
     },
     submitForm() {
       // Handle form submission logic here
