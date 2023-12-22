c49a3d79a5952c588e673bc59adc85911b33fd44
sbmTRT <thureintun@signalbase.co.jp>
2023-12-22 13:17:04 +0900

    initial commit

--- src/views/entry/Register.vue
+++ src/views/entry/Register.vue
@@ -4,19 +4,19 @@
     <div class="card-body">
       <form>
           <div class="form-group">
-              <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('input')">登録申込</button>
+              <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('input')">登録申込</button>
           </div>
           <div class="form-group">
-              <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('update')">登録事項変更届出</button>
+              <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('update')">登録事項変更届出</button>
           </div>
           <div class="form-group">
-              <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('delete')">登録抹消申出</button>
+              <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('delete')">登録抹消申出</button>
           </div>
           <div class="form-group">
-              <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('application')">利用希望申込</button>
+              <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('application')">利用希望申込</button>
           </div>
           <div class="form-group">
-              <button type="button" class="btn btn-secondary btn-sm shadow-sm">閉じる</button>
+              <button type="button" class="form-control btn btn-secondary btn-sm shadow-sm">閉じる</button>
           </div>
       </form>
     </div>
