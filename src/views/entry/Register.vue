d7ab86dfac4736dea7b7f84abcff63444481250c
sbmTRT <thureintun@signalbase.co.jp>
2023-12-22 13:12:10 +0900

    initial commit

--- src/views/entry/Register.vue
+++ src/views/entry/Register.vue
@@ -1,25 +1,25 @@
 <template>
-<div class="container-fluid col-sm-6 mt-1 mb-5">
-<div class="card">
-  <div class="card-body">
-    <div class="row mb-2 mt-5">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('input')">登録申込</button>
-    </div>
-    <div class="row mb-2">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('update')">登録事項変更届出</button>
-    </div>
-    <div class="row mb-2">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('delete')">登録抹消申出</button>
-    </div>
-    <div class="row mb-10">
-        <button type="button" class="btn btn-outline-success shadow-sm col-sm-12" @click="redirectTo('application')">利用希望申込</button>
-    </div>
-    <div class="row mb-2 col-sm-6 ml-0 mt-5">
-        <button type="button" class="btn btn-secondary btn-sm shadow-sm col-sm-12">閉じる</button>
+<body class="container-sm">
+  <div class="card">
+    <div class="card-body">
+      <div class="row mb-2 mt-5">
+          <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('input')">登録申込</button>
+      </div>
+      <div class="row mb-2">
+          <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('update')">登録事項変更届出</button>
+      </div>
+      <div class="row mb-2">
+          <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('delete')">登録抹消申出</button>
+      </div>
+      <div class="row mb-10">
+          <button type="button" class="btn btn-outline-success shadow-sm" @click="redirectTo('application')">利用希望申込</button>
+      </div>
+      <div class="row mb-2 mt-5">
+          <button type="button" class="btn btn-secondary btn-sm shadow-sm">閉じる</button>
+      </div>
     </div>
   </div>
-</div>
-</div>
+</body>
 </template>
 
 <script>
