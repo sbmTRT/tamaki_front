9912e837c98d4c06bd31086c36e7afe89e0f0ffc
sbmTRT <thureintun@signalbase.co.jp>
2023-12-22 14:03:56 +0900

    initial commit

--- src/views/entry/Input.vue
+++ src/views/entry/Input.vue
@@ -1,5 +1,6 @@
 <template>
 <body class="container-sm">
+<form>
     <div class="card">
         <div class="card-header bg-success text-white">
             登録申込
@@ -8,7 +9,6 @@
             <div class="card mt-2">
                 <div class="card-body">
                     <label class="text-success"> 申込者 </label>
-                    <form>
                         <div class="form-group">
                             <label>氏名</label>
                             <input type="text" class="form-control">
@@ -17,22 +17,31 @@
                             <label>住所</label>
                             <input type="text" class="form-control">
                         </div>
-                    </form>
                 </div>
             </div>
             <div class="card mt-2">
                 <div class="card-body">
                     <label class="text-success"> 対象物件 </label>
-                    <form>
                         <div class="form-group">
                             <button type="button" class="btn btn-success shadow-sm" @click="redirectTo('register')">登録カード入力</button>
                             <button type="button" class="btn btn-secondary shadow-sm ml-2">未入力</button>
                         </div>
-                    </form>
+                    <label class="text-success"> 契約交渉の方法 </label>
+                    <label class="text-success"> . </label>
+                    <label class="text-success"> . </label>
+                    <label class="text-success"> . </label>
+                    <label class="text-success"> . </label>
                 </div>
+            </div>
+            <div class="form-group">
+                <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('application')">登録</button>
+            </div>
+            <div class="form-group d-grid gap-2 col-8 mx-auto">
+                <button type="button" class="form-control btn btn-secondary shadow-sm mt-5" @click="redirectTo('register')">戻る</button>
             </div>
         </div>
     </div>
+</form>
 </body>
 </template>
 
