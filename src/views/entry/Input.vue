822d62c783f76a5ce9e6fb3332ed01d8c38a011c
sbmTRT <thureintun@signalbase.co.jp>
2023-12-22 17:48:07 +0900

    initial commit

--- src/views/entry/Input.vue
+++ src/views/entry/Input.vue
@@ -34,58 +34,32 @@
                 </div>
             </div>
             <div class="form-group mt-4  d-grid gap-2 col-10 mx-auto">
-                <button type="button" class="form-control btn btn-success shadow-sm" data-bs-toggle="modal" data-bs-target="#exampleModalLabel">登録</button>
+                <button type="button" class="form-control btn btn-success shadow-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">登録</button>
             </div>
             <div class="form-group d-grid gap-2 col-10 mx-auto">
                 <button type="button" class="form-control btn btn-secondary shadow-sm" @click="redirectTo('register')">戻る</button>
             </div>
         </div>
     </div>
-<!-- Confirmation Modal -->
-<!-- Modal -->
+
+    <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body" id="confirmModal">
-                    <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-md-4">.col-md-4</div>
-                                <div class="col-md-4 ms-auto">.col-md-4 .ms-auto</div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-3 ms-auto">.col-md-3 .ms-auto</div>
-                                <div class="col-md-2 ms-auto">.col-md-2 .ms-auto</div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 ms-auto">.col-md-6 .ms-auto</div>
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-9">
-                                    Level 1: .col-sm-9
-                                    <div class="row">
-                                        <div class="col-8 col-sm-6">
-                                            Level 2: .col-8 .col-sm-6
-                                        </div>
-                                        <div class="col-4 col-sm-6">
-                                            Level 2: .col-4 .col-sm-6
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
         </div>
     </div>
-</div>
+    </div>
 </form>
 </body>
 </template>
