d43a44510b899c3e3bb8d712d15f4822619bb942
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 19:19:56 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,7 @@
   <div id="app-wrapper">
     <Header />
     <div id="main-content">
-        <main>
-          <div class="row g-5">
-            <div class="col-md-7 col-lg-8">
-              <h4 class="mb-3">Billing address</h4>
-              <form class="needs-validation" novalidate>
-                <div class="row g-3">
-                  <div class="col-sm-6">
-                    <label for="firstName" class="form-label">First name</label>
-                    <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-                    <div class="invalid-feedback">
-                      Valid first name is required.
-                    </div>
-                  </div>
-
-                  <div class="col-sm-6">
-                    <label for="lastName" class="form-label">Last name</label>
-                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-                    <div class="invalid-feedback">
-                      Valid last name is required.
-                    </div>
-                  </div>
-                </div>
-
-                <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
-              </form>
-            </div>
-          </div>
-        </main>
+    <label>AAA</label>
     </div>
     <Footer />
   </div>
@@ -56,5 +29,6 @@ export default {
 
 #main-content {
   flex: 1;
+  flex-direction: column;
 }
 </style>
