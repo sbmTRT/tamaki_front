fa17999c9902eaa5f86c999aed415e17400af27c
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 16:43:47 +0900

    initial commit

--- src/views/entry/Register.vue
+++ src/views/entry/Register.vue
@@ -1,33 +1,30 @@
 <template>
-<div class="container-sm">
+<body class="container-sm">
   <div class="card">
     <div class="card-body">
       <form class="mt-5">
-          <div class="form-group ml-0">{{ message }}</div>
-          <div class="form-group mb-3">
+          <div class="form-group">
               <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('input')">登録申込</button>
           </div>
-          <div class="form-group mb-3">
+          <div class="form-group">
               <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('update')">登録事項変更届出</button>
           </div>
-          <div class="form-group mb-3">
+          <div class="form-group">
               <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('delete')">登録抹消申出</button>
           </div>
-          <div class="form-group mb-3">
+          <div class="form-group">
               <button type="button" class="form-control btn btn-outline-success shadow-sm" @click="redirectTo('application')">利用希望申込</button>
           </div>
-          <div class="form-group col-8 mx-auto">
+          <div class="form-group d-grid gap-2 col-8 mx-auto">
               <button type="button" class="form-control btn btn-secondary shadow-sm mt-5" @click="closeTab">閉じる</button>
           </div>
       </form>
     </div>
   </div>
-</div>
+</body>
 </template>
 
 <script>
-import liff from "@line/liff";
-
 export default {
   methods: {
     redirectTo(routeName) {
@@ -37,24 +34,6 @@ export default {
       window.close();
     },
   },
-  data() {
-    return {
-      message: "",
-      error: "",
-    };
-  },
-  mounted() {
-    liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID
-      })
-      .then(() => {
-        this.message = "LIFF init succeeded.";
-      }).catch((e) => {
-        this.message = "LIFF init failed.";
-        this.error = `${e}`;
-      });
-  }
 };
 </script>
 
