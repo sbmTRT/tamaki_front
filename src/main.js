bc2c46d0367f9c3d6ea6422f5043540ef5705460
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 12:08:12 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import {createApp} from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import store from './stores/store';
 import router from './routers/router';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS
-import '../node_modules/jquery/dist/jquery.min.js'; // Import Jquery
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS
+import 'jquery/dist/jquery.min.js'; // Import jQuery
 
 // Create a Vue app instance with the router and store
 const app = createApp(App);
