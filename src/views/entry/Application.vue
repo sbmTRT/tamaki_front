b1cb289c07659a22b4467bece99b4485b5fc7eea
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 15:33:52 +0900

    initial commit

--- src/views/entry/Application.vue
+++ src/views/entry/Application.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="container-fluid">
+  <body class="container-fluid">
     <form>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label>Email address</label>
+        <input type="email" >
+        <div>We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label>Email address</label>
+        <input type="email" >
+        <div>We'll never share your email with anyone else.</div>
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <div class="mb-3">
+        <label>Email address</label>
+        <input type="email" >
+        <div>We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label>Email address</label>
+        <input type="email" >
+        <div>We'll never share your email with anyone else.</div>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-  </div>
+  </body>
 </template>
 
 <script>
