478710bc5efa0aceb2dd6f88d24a8530eca63835
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 12:05:22 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
 import {createApp} from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue';
 import store from './stores/store';
 import router from './routers/router';
 
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import bootstrapcss from '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import bootstrapjs from '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS
+import jquery from  '../node_modules/jquery/dist/jquery.min.js'; // Import Jquery
 
 // Create a Vue app instance with the router and store
 const app = createApp(App);
 app.use(store);
 app.use(router);
-// Make BootstrapVue available throughout your project
-app.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-app.use(IconsPlugin)
+app.use(bootstrapcss);
+app.use(bootstrapjs);
+app.use(jquery);
 
 // Mount the app to the element with id 'app'
 app.mount('#app');
-
-
