2bec9e699d6ba722c588ff6c333f39a86c8f7e8a
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 19:14:37 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,34 @@
   <div id="app-wrapper">
     <Header />
     <div id="main-content">
-      <router-view />
+        <main>
+          <div class="row g-5">
+            <div class="col-md-7 col-lg-8">
+              <h4 class="mb-3">Billing address</h4>
+              <form class="needs-validation" novalidate>
+                <div class="row g-3">
+                  <div class="col-sm-6">
+                    <label for="firstName" class="form-label">First name</label>
+                    <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                    <div class="invalid-feedback">
+                      Valid first name is required.
+                    </div>
+                  </div>
+
+                  <div class="col-sm-6">
+                    <label for="lastName" class="form-label">Last name</label>
+                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                    <div class="invalid-feedback">
+                      Valid last name is required.
+                    </div>
+                  </div>
+                </div>
+
+                <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+              </form>
+            </div>
+          </div>
+        </main>
     </div>
     <Footer />
   </div>
