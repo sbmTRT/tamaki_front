9c536b38a975ef2d15364cbbf9c1cf163a45b331
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 16:58:09 +0900

    initial commit

--- src/views/entry/Application.vue
+++ src/views/entry/Application.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    <h2>Application Page</h2>
-    <!-- About page content goes here -->
+  <div class="container mt-5">
+    <h2>Registration Form</h2>
+    <form @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" class="form-control" id="username" v-model="formData.username" required>
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" v-model="formData.email" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" class="form-control" id="password" v-model="formData.password" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Register</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  // View-specific logic goes here
-}
+  data() {
+    return {
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission logic here
+      console.log('Form submitted with data:', this.formData);
+      // You can send the form data to the server or perform other actions
+    }
+  }
+};
 </script>
 
 <style scoped>
-/* View-specific styles go here */
+/* Add your custom styles here */
 </style>
