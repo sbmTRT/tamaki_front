dd3fcd37de73df3ad251f5ae87425d4e2d406744
sbmTRT <thureintun@signalbase.co.jp>
2023-12-26 13:35:43 +0900

    initial commit

--- src/views/entry/Input.vue
+++ src/views/entry/Input.vue
@@ -1,58 +1,12 @@
-<template><!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<div class="container-sm">
-<form>
-    <div class="card">
-        <div class="card-header bg-success text-white">
-            登録申込
-        </div>
-        <div class="card-body">
-            <div class="card mt-2">
-                <div class="card-body">
-                    <label class="text-success"> 申込者 </label>
-                        <div class="form-group">
-                            <label>氏名</label>
-                            <input type="text" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>住所</label>
-                            <input type="text" class="form-control">
-                        </div>
-                </div>
-            </div>
-            <div class="card mt-2">
-                <div class="card-body">
-                    <label class="text-success"> 対象物件 </label>
-                        <div class="form-group">
-                            <button type="button" class="btn btn-success shadow-sm" @click="redirectTo('register')">登録カード入力</button>
-                            <button type="button" class="btn btn-secondary shadow-sm ml-2">未入力</button>
-                        </div>
-                    <label class="text-success"> 契約交渉の方法 </label><br>
-                    <label class="text-success"> . </label><br>
-                    <label class="text-success"> . </label><br>
-                    <label class="text-success"> . </label><br>
-                    <label class="text-success"> . </label>
-                </div>
-            </div>
-            <div class="form-group mt-4  d-grid gap-2 col-10 mx-auto mb-3">
-                <button type="button" class="form-control btn btn-success shadow-sm" @click="redirectTo('application')">登録</button>
-            </div>
-            <div class="form-group d-grid gap-2 col-10 mx-auto">
-                <button type="button" class="form-control btn btn-secondary shadow-sm" @click="redirectTo('register')">戻る</button>
-            </div>
-        </div>
-    </div>
-</form>
+<template>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
 </div>
-</html>
-
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
 </template>
 
 <script>
