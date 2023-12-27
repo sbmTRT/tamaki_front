d0d0b651e816ff211d205e080e213c34f72cbcff
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 11:50:08 +0900

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '../routers/router.js';
 
-// // Import our custom CSS
-// import '../scss/styles.scss'
-
-// // Import only the Bootstrap components we need
-// import { Popover } from 'bootstrap';
-// Create a Vue app instance with the router and store
 const app = createApp(App);
 
 app.use(router);
 
 app.mount('#app')
-
-// // Create an example popover
-// document.querySelectorAll('[data-bs-toggle="popover"]')
-//   .forEach(popover => {
-//     new Popover(popover)
-//   })
