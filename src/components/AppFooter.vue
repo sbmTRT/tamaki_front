50292d1de45d918b55d6ab6e29a43e066e865ead
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 18:25:20 +0900

    initial commit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,11 @@
 <template>
-    <footer class="container">
+    <footer class="container mt-4">
         <p class="text-muted">Copyright &copy; 2023. All rights reserved.</p>
     </footer>
 </template>
 
 <style scoped>
 footer {
-  position: fixed;
   bottom: 0;
   width: 100%;
   padding: 10px;
