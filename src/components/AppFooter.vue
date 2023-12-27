1096abc1f371bd059c4dd57147db459d376178de
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 11:38:28 +0900

    initial commit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,4 +1,4 @@
 <template>
   <hr class="mt-5 mb-4">
-  <p class="text-muted">Copyright &copy; 2023 </strong>. All rights reserved.</p>
+  <p class="text-muted">Copyright &copy; 2023. All rights reserved.</p>
 </template>
