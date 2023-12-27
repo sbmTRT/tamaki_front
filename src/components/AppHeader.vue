68da893f26aefcd0bda70a3759ad6596cba1d65d
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 18:20:22 +0900

    initial commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="mb-4">
         <img src="../assets/img/logo.png" alt="Logo of Tamaki System">
-        <span class="ml-4"><b>玉城町空き家バンク登録システム</b></span>
+        <span>玉城町空き家バンク登録システム</span>
     </header>
 </template>
