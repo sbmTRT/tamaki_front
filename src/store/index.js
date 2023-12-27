6749082bae44eab1000a3f930193259d1c2f1147
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 15:47:42 +0900

    initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex';
 import app from './modules/app';
 
-export default createStore({
+const store = createStore({
     modules: {
         app,
     },
 });
+
+export default store;
