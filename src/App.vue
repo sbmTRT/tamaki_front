1da3fcaa500d077ea9a22e5e7dd09381bbcde1b5
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 13:13:00 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <script setup>
 import Header from './components/AppHeader.vue'
 import Footer from './components/AppFooter.vue'
+import liff from "@line/liff";
+
+const { message, error } = reactive({
+  message: "",
+  error: "",
+});
+
+export default {
+  data() {
+    return {
+      message: "",
+      error: "",
+    };
+  },
+  mounted() {
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID
+      })
+      .then(() => {
+        message.value = "LIFF init succeeded.";
+      }).catch((e) => {
+        message.value = "LIFF init failed.";
+        error.value = `${e}`;
+      });
+  }
+};
 </script>
 
 <template>
   <div class="container py-4 px-3 mx-auto">
     <Header />
-      <router-view />
+      <router-view :message="message"/>
     <Footer />
   </div>
 </template>
