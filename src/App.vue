3011967ad915a77e25d68150c0a88aacec186aef
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 13:22:45 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
 import Header from './components/AppHeader.vue'
 import Footer from './components/AppFooter.vue'
-import { useStore } from 'vuex';
-
-const store = useStore();
-
-// Dispatch the Vuex action to initialize LIFF
-store.dispatch('app/initLiff');
 </script>
 
 <template>
   <div class="container py-4 px-3 mx-auto">
     <Header />
-    <router-view />
+      <router-view/>
     <Footer />
   </div>
 </template>
