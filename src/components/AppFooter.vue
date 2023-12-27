b397b001a79eba3701bec8bf2c061ab6c1d7574c
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 18:13:20 +0900

    initial commit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,3 +3,14 @@
         <p class="text-muted">Copyright &copy; 2023. All rights reserved.</p>
     </footer>
 </template>
+
+<style scoped>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #f8f9fa; /* Add your preferred background color */
+  padding: 10px; /* Add your preferred padding */
+  text-align: center;
+}
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
-    <header>
+    <header class="mb-4">
         <img src="../assets/img/logo.png" alt="Logo of Tamaki System">
-        <span class="ml-0">玉城町空き家バンク登録システム</span>
+        <span><b>玉城町空き家バンク登録システム</b></span>
     </header>
 </template>
