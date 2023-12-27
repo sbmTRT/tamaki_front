7f9ee9569726adf7424274cd34616fa241560466
sbmTRT <thureintun@signalbase.co.jp>
2023-12-27 12:31:43 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Header from '../components/AppHeader.vue'
-import Footer from '../components/AppFooter.vue'
+import Header from './components/AppHeader.vue'
+import Footer from './components/AppFooter.vue'
 </script>
 
 <template>
