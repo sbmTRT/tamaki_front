19acd059b40830dadd5372602ad081f1a514ba2b
sbmTRT <thureintun@signalbase.co.jp>
2023-12-28 12:51:35 +0900

    initial commit

--- src/views/entry/Update.vue
+++ src/views/entry/Update.vue
@@ -1,41 +1,6 @@
-
-<script>
-export default {
-  // View-specific logic goes here
-}
-</script>
-
-<style scoped>
-/* View-specific styles go here */
-</style>
-
 <template>
-  <main>
-    <div class="row g-5">
-      <div class="col-md-7 col-lg-8">
-        <h4 class="mb-3">Billing address</h4>
-        <form class="needs-validation" novalidate>
-          <div class="row g-3">
-            <div class="col-sm-6">
-              <label for="firstName" class="form-label">First name</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Valid first name is required.
-              </div>
-            </div>
-
-            <div class="col-sm-6">
-              <label for="lastName" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Valid last name is required.
-              </div>
-            </div>
-          </div>
-
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
-        </form>
-      </div>
-    </div>
-  </main>
+  <div>
+    <h2>Update Page</h2>
+    <!-- About page content goes here -->
+  </div>
 </template>
--- src/views/member/Add.vue
+++ src/views/member/Add.vue
@@ -4,13 +4,3 @@
     <!-- About page content goes here -->
   </div>
 </template>
-
-<script>
-export default {
-  // View-specific logic goes here
-}
-</script>
-
-<style scoped>
-/* View-specific styles go here */
-</style>
